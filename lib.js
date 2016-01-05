3ad7839cba1633349a9d8a79b05f552db7ecf546
Seth <aoiinfin@gmail.com>
2016-01-05 10:33:40 -0600

    Create lib.js
    classes

---
+++ lib.js
@@ -0,0 +1,12 @@
+class _Config {
+
+	constructor(stringToFormat, bold, italic, strike, underline, lineBreak) {
+  	this._stringToFormat = stringToFormat;
+  	this._boldSymbol = bold;
+    this._italicSymbol = italic;
+    this._strikeSymbol = strike;
+    this._underlineSymbol = underline;
+    this._lineBreakSymbol = lineBreak;
+  }
+  
+}
