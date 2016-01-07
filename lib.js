08434c91edb5f6cca48e9cc244f2db8b12e59336
Seth <aoiinfin@gmail.com>
2016-01-07 13:38:36 -0600

    Rewriting with closures:
    left off on line 35

--- lib.js
+++ lib.js
@@ -8,42 +8,49 @@
   }
   return false;
  }
-  var input = (function() {
-    //getElementById... 
-    var string = '++testing++';
+  var InputClosure = function(formatString) {
+    var input = {};
+    input.formatString = formatString;
 
-    return string;
-  })();
+    input.getFormatString = function getString() {
+      return input;
+    }
+  }
 
-  var symbolCreation = (function() {
-      var defaultSymbols = {bold: '++', italic: '**', strike: '--', underline: '__', linebreak : '>>'};
-      var symbols = defaultSymbols;
+  var symbolCreationClosure = function() {
+      var symbolCreation = {};
+      symbolCreation.formatSymbols = {bold: '++', italic: '**', strike: '--', underline: '__', linebreak : '>>'};
+      
+      symbolCreation.setSymbols = function(bold, italic, strike, underline, linebreak) {
 
-      return symbols;
-  })();
+      }
+
+      symbolCreation.getSymbols = function() {
+        return this.formatSymbols;
+      }
+      return symbolCreation;
+  }
 
 
-  var symbolLocations = (function() {
-    var formatString = '++testing++';
-    var positionObj;
-    var symArr = [];
+  var SymbolLocationsClosure = function(string, key) {
+    symbolLocations = {}
+
+    symbolLocations.formatString = key;
+    symbolLocations.positionObj;
+    symbolLocations.symArr = [];
     for (value in symbolCreation) {
       symArr.pop(value);
     };
 
-    if (formatString != undefined) {
-      for (i in formatString) {
-        if (symArr.contains(symArr, formatString[i]) && symArr.contains(symArr, formatString[i+1])) {
-          positionObj.bold = formatString[i] == symbolCreation.bold && formatString[i+1] == symbolCreation.bold ? positionObj.bold.push([i, i+1]) : positionObj.bold;
-          positionObj.italic = formatString[i] == symbolCreation.italic && formatString[i+1] == symbolCreation.italic ? positionObj.italic.push([i, i+1]) : positionObj.italic;
-          positionObj.strike = formatString[i] == symbolCreation.strike && formatString[i+1] == symbolCreation.strike ? positionObj.strike.push([i, i+1]) : positionObj.strike;
-          positionObj.underline = formatString[i] == symbolCreation.underline && formatString[i+1] == symbolCreation.underline ? positionObj.underline.push([i, i+1]) : positionObj.underline;
-          positionObj.linebreak = formatString[i] == symbolCreation.linebreak && formatString[i+1] == symbolCreation.linebreak ? positionObj.linebreak.push([i, i+1]) : positionObj.linebreak;
-          i += 1;
-        }
+    for (i in formatString) {
+      if (symbolLocations.symArr.contains(symbolLocations.symArr, formatString[i]) && symbolLocations.symArr.contains(symbolLocations.symArr, formatString[i+1])) {
+        positionObj.bold = formatString[i] == symbolCreation.bold && formatString[i+1] == symbolCreation.bold ? positionObj.bold.push([i, i+1]) : positionObj.bold;
+        positionObj.italic = formatString[i] == symbolCreation.italic && formatString[i+1] == symbolCreation.italic ? positionObj.italic.push([i, i+1]) : positionObj.italic;
+        positionObj.strike = formatString[i] == symbolCreation.strike && formatString[i+1] == symbolCreation.strike ? positionObj.strike.push([i, i+1]) : positionObj.strike;
+        positionObj.underline = formatString[i] == symbolCreation.underline && formatString[i+1] == symbolCreation.underline ? positionObj.underline.push([i, i+1]) : positionObj.underline;
+        positionObj.linebreak = formatString[i] == symbolCreation.linebreak && formatString[i+1] == symbolCreation.linebreak ? positionObj.linebreak.push([i, i+1]) : positionObj.linebreak;
+        i += 1;
       }
-    } else {
-      console.log('fuck');
     }
 
 
@@ -58,10 +65,9 @@
       }
 
     }
-  })();
-  console.log(symbolLocations.getPositon());
-  var symbolReplacement;
+  }
 
 
 
-})();
+
+}
