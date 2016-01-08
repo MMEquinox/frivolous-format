6c38e1d62a23fe3e27c9450addb4c99cef59990d
Seth <aoiinfin@gmail.com>
2016-01-08 09:09:02 -0600

    Update frivolous-format.js

--- frivolous-format.js
+++ frivolous-format.js
@@ -31,32 +31,38 @@
       return symbolCreation;
   }
 
-  var x = symbolCreationClosure();
-  console.log(x.getSymbols());
-
 
   var SymPosClosure = function(string, key) {
     symPos = {};
 
     symPos.formatString = string;
     symPos.formatKey = new Array;
-    symPos.formatKey = (function() {
+    (function() {
       for (value in key) {
         symPos.formatKey.push(value);
-        console.log(key);
       }
     })();
-    symPos.poss;
+
+    symPos.poss = {
+      bold : new Array,
+      italic : new Array,
+      strike : new Array,
+      underline : new Array,
+      linebreak : new Array
+    };
 
 
     for (i = 0; i < symPos.formatString.length; i++) {
-      for (z = 0; i < symPos.formatKey; i++) {
-        if (symPos.formatString[i] == symPos.formatKey[z] && symPos.formatString[i+1] == symPos.formatKey[z]){
-          symPos.poss.bold = symPos.formatKey[z] == string.bold ? console.log('bold') : symPos.poss.bold;
-          symPos.poss.italic = symPos.formatKey[z] == string.italic ? symPos.poss.push([i, i+1]) : symPos.poss.bold;
-          symPos.poss.strike = symPos.formatKey[z] == string.strike ? symPos.poss.push([i, i+1]) : symPos.poss.bold;
-          symPos.poss.underline = symPos.formatKey[z] == string.underline ? symPos.poss.push([i, i+1]) : symPos.poss.bold;
-          symPos.poss.linebreak = symPos.formatKey[z] == string.linebreak ? symPos.poss.push([i, i+1]) : symPos.poss.bold;
+      console.log(i);
+      for (z = 0; z < symPos.formatKey.length; z++) {
+        console.log(z);
+        if (symPos.formatString[i] == symPos.formatKey[z] && symPos.formatString[i+1] == symPos.formatKey[z]) {
+          console.log('working');
+          symPos.poss.bold = symPos.formatKey[z] == string.bold ? symPos.poss.bold.push(i, i+1) : symPos.poss.bold;
+          symPos.poss.italic = symPos.formatKey[z] == string.italic ? symPos.poss.italic.push([i, i+1]) : symPos.poss.bold;
+          symPos.poss.strike = symPos.formatKey[z] == string.strike ? symPos.poss.strike.push([i, i+1]) : symPos.poss.bold;
+          symPos.poss.underline = symPos.formatKey[z] == string.underline ? symPos.underline.push([i, i+1]) : symPos.poss.bold;
+          symPos.poss.linebreak = symPos.formatKey[z] == string.linebreak ? symPos.linebreak.push([i, i+1]) : symPos.poss.bold;
         }
       }
     }
@@ -70,6 +76,4 @@
 
   var symbols = symbolCreationClosure();
   var pos = SymPosClosure('++Format++ this __string__!',symbols.getSymbols());
-  console.log(pos.getPos());
-  console.log(pos.formatKey);
 })();
